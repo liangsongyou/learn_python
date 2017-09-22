da585542f5eaf445e67f80212feaf6cb3bdd266b
liangsongyou <29649017+liangsongyou@users.noreply.github.com>
2017-09-22 03:25:05 -0500

    Add files via upload

---
+++ code/ThinkPython10_11.py
@@ -0,0 +1,23 @@
+from __future__ import print_function, division
+
+
+from ThinkPython10_10 import in_bisect, make_word_list
+
+
+def reverse_pair(word_list, word):
+	"""Checks whether a reversed word appears in word_list.
+
+	word_list: list of strings
+	word:string
+	"""
+
+	rev_word = word[::-1]
+	return in_bisect(word_list, rev_word)
+
+
+if __name__ == '__main__':
+	word_list = make_word_list()
+
+	for word in word_list:
+		if reverse_pair(word_list, word):
+			print(word, word[::-1])
