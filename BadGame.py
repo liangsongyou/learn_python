f67efd06c91b7f931a60399d069966f2e4ce04d5
liangsongyou <29649017+liangsongyou@users.noreply.github.com>
2017-09-22 01:29:26 -0500

    Add files via upload

---
+++ BadGame.py
@@ -0,0 +1,14 @@
+
+print('There is five door. You can choose one of them. Whicn one do you choose?')
+Firt_Chose = input(' ')
+t = Firt_Chose
+
+if t:
+	print('Welcome! Here are five room too, and which one do you choose?')
+	Second_Choise = input(' ')
+else:
+	print('Why do not choose any one? You dead.')
+
+First_room()
+
+
